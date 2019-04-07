1afc417c06e0143f3ea09b320d71c0d41cd8fa5e
michaelost <mikhaylo.ostroviy@gmail.com>
2019-04-07 23:35:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const convertArrowFunction = (str) => {
   return `${head} { return ${body}; }`;
 }
 
+const toJavascript = (str) => {
+  const regExp = /^[A-Za-z]*\s/;
+  const prop = str.match(regExp, '');
+  const value = str.replace(regExp, '');
+  if (!prop || !prop.length || !value) {
+    return '';
+  }
+  return `  ${prop[0].toLowerCase()}: "${value}",`
+}
+
+const getWrappedArray = (array) => ['{', ...array, '}'];
+
 module.exports = plugin => {
   plugin.setOptions({ dev: true });
   plugin.registerCommand('EchoMessage', async () => {
@@ -26,6 +38,35 @@ module.exports = plugin => {
         });
       }
     }, { sync: false });
+
+
+  plugin.registerCommand('TOJS', async () => {
+      try {
+        const startSelection = await plugin.nvim.eval("getpos(\"'<\")");
+        const endSelection =  await plugin.nvim.eval("getpos(\"'>\")");
+        const lineStart = startSelection[1];
+        const lineEnd = endSelection[1];
+        const lines = await plugin.nvim.buffer.getLines({ start: lineStart, end: lineEnd });
+        const res = lines
+          .map(el => toJavascript(el))
+          .filter(el => el);
+
+        const wrapped = getWrappedArray(res);
+
+        fs.writeFile(os.homedir() + '/vimerror.txt', JSON.stringify(wrapped), function (err) {
+          if (err) throw err;
+        });
+
+        await plugin.nvim.buffer.replace(wrapped, lineStart -1); 
+
+      } catch (err) {
+        fs.writeFile(os.homedir() + '/vimerror.txt', JSON.stringify(err.message), function (err) {
+          if (err) throw err;
+        });
+      }
+    }, { sync: false });
+
+
 
   plugin.registerFunction('SetLines',() => {
     return plugin.nvim.setLine('May I offer you an egg in these troubling times')
