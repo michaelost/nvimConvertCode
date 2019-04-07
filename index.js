4443c026570e5273d5fa47051757b7f6ca20df34
michaelost <mikhaylo.ostroviy@gmail.com>
2019-04-07 22:56:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
+const fs = require('fs');
+const os = require('os');
+
+const convertArrowFunction = (str) => {
+  const regExp = /(\()([a-zA-z\s0-9]{1,}\))(\s\=\>\s)/;
+  const body = str.replace(regExp, '');
+  const head = str.match(regExp)[0];
+  return `${head} { return ${body}; }`;
+}
+
 module.exports = plugin => {
-  plugin.setOptions({ dev: false });
+  plugin.setOptions({ dev: true });
   plugin.registerCommand('EchoMessage', async () => {
       try {
         const startSelection = await plugin.nvim.eval("getpos(\"'<\")");
         const endSelection =  await plugin.nvim.eval("getpos(\"'>\")");
-
         const lineStart = startSelection[1];
         const lineEnd = endSelection[1];
-
         const line = await plugin.nvim.getLine(lineStart, lineEnd);
-
-        await plugin.nvim.outWrite(line);
+        const newString = convertArrowFunction(line);
+        await plugin.nvim.buffer.replace([newString], lineStart -1); 
+        await plugin.nvim.outWrite('fdsfsdasadfdf');
       } catch (err) {
-
-        await plugin.nvim.outWrite('err');
-        console.error(err);
+        fs.writeFile(os.homedir() + '/vimerror.txt', JSON.stringify(err.message), function (err) {
+          if (err) throw err;
+        });
       }
     }, { sync: false });
 
